e2fc9a1e82a5b1669e7e3d89a23d57bade193eda
Louis Vassaux <louis.vassaux@sfr.fr>
2018-02-28 17:37:43 +0100

    dsq

---
+++ SuperBulletManiaReloadedTheSequel/CollisionManager.cs
@@ -0,0 +1,21 @@
+﻿using MonoGame.FZT;
+
+namespace SuperBulletManiaReloadedTheSequel
+{
+    class CollisionManager
+    {
+        public CollisionManager() { }
+
+        public void PlayerAndWall(Player player_, bool isVertical_, float pos_)
+        {
+            FRectangle hb = player_.MovHB()[0];
+            if (isVertical_)
+            {
+                if (hb.X + hb.Width < pos_ && pos_ < hb.MovX().X + hb.Width)
+                    player_.mov.X -= hb.MovX().X + hb.Width - pos_;
+                else if (hb.X > pos_ && pos_ > hb.MovX().X)
+                    player_.mov.X += pos_ - hb.MovX().X;
+            }
+        }
+    }
+}
--- SuperBulletManiaReloadedTheSequel/Game1.cs
+++ SuperBulletManiaReloadedTheSequel/Game1.cs
@@ -5,6 +5,7 @@ using MonoGame.FZT;
 using MonoGame.FZT.Assets;
 using MonoGame.FZT.Data;
 using MonoGame.FZT.Drawing;
+using MonoGame.FZT.Input;
 using System.Collections.Generic;
 
 namespace SuperBulletManiaReloadedTheSequel
@@ -15,15 +16,20 @@ namespace SuperBulletManiaReloadedTheSequel
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         Player player;
+        InputProfile inputProfile;
         
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+            graphics.PreferredBackBufferHeight = 1080;
+            graphics.PreferredBackBufferWidth = 1920;
         }
 
         protected override void Initialize()
         {
+            inputProfile = new InputProfile(new KeyManager[] { new KeyManager(Keys.Left, "playerLeft"), new KeyManager(Keys.Right, "playerRight"), new KeyManager(Keys.Up, "playerUp"), new KeyManager(Keys.Down, "playerDown") });
+
             base.Initialize();
         }
         
@@ -33,7 +39,7 @@ namespace SuperBulletManiaReloadedTheSequel
 
             TextureDrawer playerDrawer = new TextureDrawer(Content.Load<Texture2D>("greenBeamChunk"), new HitboxCollection[] { new HitboxCollection(new FRectangle[][] { new FRectangle[] { new FRectangle(0, 0, 16, 16) } }, "iDunno") }, "default");
             DrawerCollection drawer = new DrawerCollection(new TextureDrawer[] { playerDrawer }, "playerDrawThingy");
-            player = new Player(drawer, new Vector2(800, 800), new List<Property>());
+            player = new Player(drawer, new Vector2(0, 0), new List<Property>());
         }
         
         protected override void UnloadContent()
@@ -46,6 +52,9 @@ namespace SuperBulletManiaReloadedTheSequel
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
+            inputProfile.Update(Keyboard.GetState(), GamePad.GetState(1));
+
+            player.Input(ConvertInput());
             player.Update((float)gameTime.ElapsedGameTime.TotalSeconds);
 
             base.Update(gameTime);
@@ -53,12 +62,26 @@ namespace SuperBulletManiaReloadedTheSequel
         
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.White);
             spriteBatch.Begin();
             player.Draw(spriteBatch);
             spriteBatch.End();
 
             base.Draw(gameTime);
+        }
+
+        protected Vector2 ConvertInput()
+        {
+            Vector2 inputVec = Vector2.Zero;
+            if (inputProfile.Pressed("playerLeft"))
+                inputVec.X -= 1;
+            else if (inputProfile.Pressed("playerRight"))
+                inputVec.X += 1;
+            if (inputProfile.Pressed("playerUp"))
+                inputVec.Y -= 1;
+            else if (inputProfile.Pressed("playerDown"))
+                inputVec.Y += 1;
+            return inputVec;
         }
     }
 }
