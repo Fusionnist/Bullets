e4252e27ba3c27f816f73b49436b4060ae2ed1c8
Louis Vassaux <louis.vassaux@sfr.fr>
2018-04-20 21:35:14 +0200

    dsq

--- SuperBulletManiaReloadedTheSequel/Game1.cs
+++ SuperBulletManiaReloadedTheSequel/Game1.cs
@@ -6,8 +6,10 @@ using MonoGame.FZT.Assets;
 using MonoGame.FZT.Data;
 using MonoGame.FZT.Drawing;
 using MonoGame.FZT.Input;
+using MonoGame.FZT.UI;
 using System;
 using System.Collections.Generic;
+using static SuperBulletManiaReloadedTheSequel.Enums;
 
 namespace SuperBulletManiaReloadedTheSequel
 {
@@ -18,43 +20,33 @@ namespace SuperBulletManiaReloadedTheSequel
         SpriteBatch spriteBatch;
         Player player;
         InputProfile inputProfile;
-        CollisionManager colman;
-        BulletShooter testShooter;
-        int boundWidth, boundHeight;
+        CursorManager mouseMan;
+        GamePhase phase;
+        UISystem currentUI;
+        UISystem[] UIs;
         
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
-            boundHeight = 810;
-            boundWidth = 1440;
-            graphics.PreferredBackBufferHeight = boundHeight;
-            graphics.PreferredBackBufferWidth = boundWidth;
         }
 
         protected override void Initialize()
         {
             inputProfile = new InputProfile(new KeyManager[] { new KeyManager(Keys.Left, "playerLeft"), new KeyManager(Keys.Right, "playerRight"), new KeyManager(Keys.Up, "playerUp"), new KeyManager(Keys.Down, "playerDown") });
-            colman = new CollisionManager(boundWidth, boundHeight);
-
+            phase = GamePhase.Menu;
+            mouseMan = new CursorManager();
+            IsMouseVisible = true;
             base.Initialize();
         }
         
         protected override void LoadContent()
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            HitboxCollection tempColHb = new HitboxCollection(new FRectangle[][] { new FRectangle[] { new FRectangle(0, 0, 16, 16) } }, "collision");
-            HitboxCollection tempHurtHb = new HitboxCollection(new FRectangle[][] { new FRectangle[] { new FRectangle(0, 0, 16, 16, properties_: new List<Property>() { new Property("damage", "1", "damage") }) } }, "hurt");
-
-            TextureDrawer playerDrawer = new TextureDrawer(Content.Load<Texture2D>("greenBeamChunk"), new HitboxCollection[] { tempColHb }, "default");
-            DrawerCollection drawer = new DrawerCollection(new TextureDrawer[] { playerDrawer }, "playerDrawThingy");
-            player = new Player(drawer, new Vector2(0, 0), new List<Property>());
-
-            TextureDrawer testBulletDrawer = new TextureDrawer(Content.Load<Texture2D>("dot"), new HitboxCollection[] { tempColHb, tempHurtHb }, "default");
-            DrawerCollection testBulletDrawerCol = new DrawerCollection(new TextureDrawer[] { testBulletDrawer }, "bulletDrawThingy");
-            Bullet baseBullet = new Bullet(testBulletDrawerCol, new Vector2(boundWidth / 2, boundHeight / 2), new List<Property>(), 0);
-            testShooter = new BulletShooter(drawer, new Vector2(boundWidth / 2, boundHeight / 2), new List<Property>(), 0, baseBullet, 0.1f, 0.5f);
+            Button button = new Button("goToGame", new Rectangle(100, 100, 16, 16), new TextureDrawer(Content.Load<Texture2D>("dot")));
+            Button button2 = new Button("goToMenu", new Rectangle(50, 50, 16, 16), new TextureDrawer(Content.Load<Texture2D>("dot")));
+            UIs = new UISystem[] { new UISystem(new List<Button>(1) {button}, "Menu"), new UISystem(new List<Button>(1) { button2 }, "Game") };
+            currentUI = UIs[0];
         }
         
         protected override void UnloadContent()
@@ -64,27 +56,12 @@ namespace SuperBulletManiaReloadedTheSequel
         
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
-
-            inputProfile.Update(Keyboard.GetState(), GamePad.GetState(1));
-
-            player.Input(ConvertInput());
-            player.MultMov((float)gameTime.ElapsedGameTime.TotalSeconds);
-            colman.TileAndPlayer(testShooter, player);
-
-            foreach (var testBullet in testShooter.bullets)
+            currentUI.HandleMouseInput(mouseMan);
+            for (int i = 0; i < UIs.Length; i++)
             {
-                testBullet.MultMov((float)gameTime.ElapsedGameTime.TotalSeconds);
-                colman.TileAndBullet(testBullet, testShooter);
+                if (currentUI.IssuedCommand("goTo" + UIs[i].Name))
+                    currentUI = UIs[i];
             }
-            colman.DoBounds(player, testShooter.bullets.ToArray());
-
-            foreach (var testBullet in testShooter.bullets)
-                testBullet.Update((float)gameTime.ElapsedGameTime.TotalSeconds);
-            testShooter.Update((float)gameTime.ElapsedGameTime.TotalSeconds);
-            player.Update((float)gameTime.ElapsedGameTime.TotalSeconds);
-
             base.Update(gameTime);
         }
         
@@ -92,27 +69,10 @@ namespace SuperBulletManiaReloadedTheSequel
         {
             GraphicsDevice.Clear(Color.White);
             spriteBatch.Begin();
-            player.Draw(spriteBatch);
-            testShooter.Draw(spriteBatch);
-            foreach (var testBullet in testShooter.bullets)
-                testBullet.Draw(spriteBatch);
+            currentUI.Draw(spriteBatch);
             spriteBatch.End();
 
             base.Draw(gameTime);
-        }
-
-        protected Vector2 ConvertInput()
-        {
-            Vector2 inputVec = Vector2.Zero;
-            if (inputProfile.Pressed("playerLeft"))
-                inputVec.X -= 1;
-            else if (inputProfile.Pressed("playerRight"))
-                inputVec.X += 1;
-            if (inputProfile.Pressed("playerUp"))
-                inputVec.Y -= 1;
-            else if (inputProfile.Pressed("playerDown"))
-                inputVec.Y += 1;
-            return inputVec;
         }
     }
 }
