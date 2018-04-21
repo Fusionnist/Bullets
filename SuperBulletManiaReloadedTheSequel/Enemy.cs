7192ce8ae4a144dd8fdd82c7a297eaaca21c114b
Fusionnist <antoine.chartier@gmail.com>
2018-04-21 12:15:10 +0200

    ass

--- SuperBulletManiaReloadedTheSequel/Enemy.cs
+++ SuperBulletManiaReloadedTheSequel/Enemy.cs
@@ -15,10 +15,12 @@ namespace SuperBulletManiaReloadedTheSequel
     {
         //next pt in map
         Vector2 point;
+        Timer deathTimer;
 
         public Enemy(DrawerCollection t_, Vector2 p_, List<Property> prop_) : base(t_,p_,prop_,"enemi","enemy")
         {
             point = Vector2.Zero;
+            deathTimer = new Timer(10);
         }
 
         public override void FeedVector(Vector2 vec_, string context_)
@@ -33,21 +35,46 @@ namespace SuperBulletManiaReloadedTheSequel
 
         public override void Move()
         {
+            if (!isDestroyed)
+            {
+                FollowPoint();
+                Wiggle();
+            }
+            base.Move();
+        }
+
+        void FollowPoint()
+        {
             //FOLLOW PATH
             Vector2 v = (point - pos);
             v.Normalize();
             mov += v;
+        }
 
+        void Wiggle()
+        {
             //WIGGLE
-            foreach(Entity e in EntityCollection.GetGroup("enemy"))
+            foreach (Entity e in EntityCollection.GetGroup("enemy"))
             {
-                if((e.pos - pos).Length() < 10)
+                if ((e.pos - pos).Length() < 10)
                 {
                     mov += (e.pos - pos) / 10;
                 }
             }
+        }
 
-            base.Move();
+        public override void Update(float elapsedTime_)
+        {
+            if(hp < 0)
+            {
+                isDestroyed = true;
+                deathTimer.Update(elapsedTime_);
+                if (deathTimer.Complete())
+                {
+                    exists = false;
+                }
+            }
+            base.Update(elapsedTime_);
         }
     }
 }
