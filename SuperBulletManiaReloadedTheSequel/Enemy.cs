a754a5e2b56d1a12f918f9f1f5bc79f8e120918e
Fusionnist <antoine.chartier@gmail.com>
2018-04-21 11:53:56 +0200

    a

---
+++ SuperBulletManiaReloadedTheSequel/Enemy.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGame.FZT;
+using MonoGame.FZT.Assets;
+using MonoGame.FZT.Data;
+using MonoGame.FZT.Drawing;
+using MonoGame.FZT.Input;
+using System;
+using System.Collections.Generic;
+
+namespace SuperBulletManiaReloadedTheSequel
+{
+    public class Enemy : Entity
+    {
+        //next pt in map
+        Vector2 point;
+
+        public Enemy(DrawerCollection t_, Vector2 p_, List<Property> prop_) : base(t_,p_,prop_,"enemi","enemy")
+        {
+            point = Vector2.Zero;
+        }
+
+        public override void FeedVector(Vector2 vec_, string context_)
+        {
+            //REGISTER PATH POINT
+            if(context_ == "follow")
+            {
+                point = vec_;
+            }
+            base.FeedVector(vec_, context_);
+        }
+
+        public override void Move()
+        {
+            //FOLLOW PATH
+            Vector2 v = (point - pos);
+            v.Normalize();
+            mov += v;
+
+            //WIGGLE
+            foreach(Entity e in EntityCollection.GetGroup("enemy"))
+            {
+                if((e.pos - pos).Length() < 10)
+                {
+                    mov += (e.pos - pos) / 10;
+                }
+            }
+
+            base.Move();
+        }
+    }
+}
---
+++ SuperBulletManiaReloadedTheSequel/Map.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGame.FZT;
+using MonoGame.FZT.Assets;
+using MonoGame.FZT.Data;
+using MonoGame.FZT.Drawing;
+using MonoGame.FZT.Input;
+using System;
+using System.Collections.Generic;
+
+namespace SuperBulletManiaReloadedTheSequel
+{
+    public class Map
+    {
+        public Vector2[] points;
+        TextureDrawer ground;
+        public FRectangle[] buildableAreas;
+
+        public Map(Vector2[] points_, TextureDrawer ground_, FRectangle[] buildableAreas_)
+        {
+            points = points_;
+            ground = ground_;
+            buildableAreas = buildableAreas_;
+        }
+
+        public Vector2 NextPoint(Vector2 entPos_)
+        {
+            int closestIndex = 0;
+            float record = 10000;
+            for(int x = 0; x < points.Length; x++)
+            {
+                if((points[x]-entPos_).Length() < record)
+                {
+                    closestIndex = x;
+                    record = (points[x] - entPos_).Length();
+                }
+            }
+            if(closestIndex + 1 != points.Length)
+                return points[closestIndex + 1];
+            else { return points[closestIndex]; }
+        }
+
+        public void Update(float es_)
+        {
+            //update ground texture
+            ground.Update(es_);
+            //direct enemies
+            foreach (Entity e in EntityCollection.GetGroup("enemy"))
+            {
+                e.FeedVector(NextPoint(e.pos), "path");
+            }
+        }
+
+        public void Draw(SpriteBatch sb_)
+        {
+            //draw map
+            ground.Draw(sb_, Vector2.Zero);
+        }
+    }
+}
